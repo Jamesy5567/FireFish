d3ffc4f30aa0a42a6a916c6d1c2ca90dedad841a
Jamesy5567 <jamesgriffith724@gmail.com>
2024-10-21 09:23:45 +0100

    Init Commit

---
+++ FireFish_Client/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FireFish_Client.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
